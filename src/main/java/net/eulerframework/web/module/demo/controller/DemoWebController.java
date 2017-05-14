c02c2b8031b9870776282eb5ba5fd7f67c222263
cFrost <cFrost@eulerproject.io>
2017-05-14 23:53:01 +0800

    Add Code

---
+++ src/main/java/net/eulerframework/web/module/demo/controller/DemoWebController.java
@@ -0,0 +1,17 @@
+package net.eulerframework.web.module.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import net.eulerframework.web.core.annotation.WebController;
+import net.eulerframework.web.core.base.controller.JspSupportWebController;
+
+@WebController
+@RequestMapping("/")
+public class DemoWebController extends JspSupportWebController {
+    
+    @RequestMapping(value = { "", "/", "index" }, method = RequestMethod.GET)
+    public String index() {        
+        return this.redirect("/dashboard");
+    }
+}
---
+++ src/main/java/net/eulerframework/web/module/demo/controller/api/AuthenticationApi.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package net.eulerframework.web.module.demo.controller.api;
+
+import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import net.eulerframework.web.config.WebConfig;
+import net.eulerframework.web.core.annotation.ApiEndpoint;
+import net.eulerframework.web.core.base.controller.AbstractApiEndpoint;
+import net.eulerframework.web.module.authentication.service.IAuthenticationService;
+
+/**
+ * @author cFrost
+ *
+ */
+@ApiEndpoint
+@RequestMapping("/")
+public class AuthenticationApi extends AbstractApiEndpoint {
+
+    @Resource
+    private IAuthenticationService authenticationService;
+    
+    @RequestMapping(value = "clean") 
+    public void clean() {
+        WebConfig.clearWebConfigCache();
+    }
+    
+    @RequestMapping(value = "test") 
+    public void test() {
+         System.out.println(WebConfig.getAdminJspPath());
+         System.out.println(WebConfig.getAdminRootPath());
+         System.out.println(WebConfig.getApiAuthenticationType());
+         System.out.println(WebConfig.getApiRootPath());
+         System.out.println(WebConfig.getEmailFormat());
+         System.out.println(WebConfig.getI18nRefreshFreq());
+         System.out.println(WebConfig.getJspPath());
+         System.out.println(WebConfig.getMinPasswordLength());
+         System.out.println(WebConfig.getMultiPartConfig());
+         System.out.println(WebConfig.getOAuthSeverType());
+         System.out.println(WebConfig.getPasswordFormat());
+         System.out.println(WebConfig.getRamCacheCleanFreq());
+         System.out.println(WebConfig.getUploadPath());
+         System.out.println(WebConfig.getUserContextCacheLife());
+         System.out.println(WebConfig.getUsernameFormat());
+         System.out.println(WebConfig.getWebAuthenticationType());
+        
+    }
+
+}
