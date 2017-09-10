aea3796529d3c8fa0c6ac1638644b2d8fb2ff251
cFrost <cFrost@eulerproject.io>
2017-09-10 23:03:20 +0800

    删除多余代码

--- src/main/java/net/eulerframework/web/module/demo/controller/DemoWebController.java
+++ src/main/java/net/eulerframework/web/module/demo/controller/DemoWebController.java
@@ -5,13 +5,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import net.eulerframework.web.core.annotation.WebController;
 import net.eulerframework.web.core.base.controller.JspSupportWebController;
+import net.eulerframework.web.module.authentication.context.UserContext;
 
 @WebController
 @RequestMapping("/")
 public class DemoWebController extends JspSupportWebController {
     
     @RequestMapping(value = { "", "/", "index" }, method = RequestMethod.GET)
-    public String index() {        
+    public String index() {
+        System.out.println(UserContext.getCurrentUser().getUsername());
         return this.redirect("/dashboard");
     }
 }
--- src/main/java/net/eulerframework/web/module/demo/controller/api/AuthenticationApi.java
+++ src/main/java/net/eulerframework/web/module/demo/controller/api/AuthenticationApi.java
@@ -24,22 +24,6 @@ public class AuthenticationApi extends AbstractApiEndpoint {
     
     @RequestMapping(value = "test") 
     public void test() {
-         System.out.println(WebConfig.getAdminJspPath());
-         System.out.println(WebConfig.getAdminRootPath());
-         System.out.println(WebConfig.getApiAuthenticationType());
-         System.out.println(WebConfig.getApiRootPath());
-         System.out.println(WebConfig.getEmailFormat());
-         System.out.println(WebConfig.getI18nRefreshFreq());
-         System.out.println(WebConfig.getJspPath());
-         System.out.println(WebConfig.getMinPasswordLength());
-         System.out.println(WebConfig.getMultiPartConfig());
-         System.out.println(WebConfig.getOAuthSeverType());
-         System.out.println(WebConfig.getPasswordFormat());
-         System.out.println(WebConfig.getRamCacheCleanFreq());
-         System.out.println(WebConfig.getUploadPath());
-         System.out.println(WebConfig.getUserContextCacheLife());
-         System.out.println(WebConfig.getUsernameFormat());
-         System.out.println(WebConfig.getWebAuthenticationType());
         
     }
 
