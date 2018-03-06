d6a9765daa5d9e4b4567bc3349f46ca689aa2ffd
cFrost <cFrost@eulerproject.io>
2018-03-06 16:22:12 +0800

    replace tab with 4 spaces

--- demo-spring-boot/src/main/java/net/eulerframework/web/StartWebApplication.java
+++ demo-spring-boot/src/main/java/net/eulerframework/web/StartWebApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StartWebApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StartWebApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StartWebApplication.class, args);
+    }
 }
