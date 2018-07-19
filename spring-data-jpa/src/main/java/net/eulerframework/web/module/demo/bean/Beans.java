821461eb5e895a1476a3b25a25e3bc6bbe0ae68b
cFrost <cFrost@eulerproject.io>
2018-07-20 01:20:42 +0800

    del redis bean

--- spring-data-jpa/src/main/java/net/eulerframework/web/module/demo/bean/Beans.java
+++ spring-data-jpa/src/main/java/net/eulerframework/web/module/demo/bean/Beans.java
@@ -2,57 +2,9 @@ package net.eulerframework.web.module.demo.bean;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisPassword;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import redis.clients.jedis.JedisPoolConfig;
 
 @Configuration
 public class Beans {
-
-    @Bean
-    public JedisPoolConfig getJedisPoolConfig() {
-        JedisPoolConfig JedisPoolConfig = new JedisPoolConfig();
-        JedisPoolConfig.setMaxIdle(1000);
-        JedisPoolConfig.setMaxTotal(100);
-        JedisPoolConfig.setMinIdle(100);
-        return JedisPoolConfig;
-    }
-    
-    @Bean
-    public RedisStandaloneConfiguration getRedisStandaloneConfiguration() {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        redisStandaloneConfiguration.setDatabase(0);
-        redisStandaloneConfiguration.setHostName("127.0.0.1");
-        redisStandaloneConfiguration.setPassword(RedisPassword.none());
-        redisStandaloneConfiguration.setPort(6379);
-        return redisStandaloneConfiguration;
-    }
-
-    @Bean
-    public JedisConnectionFactory getJedisConnectionFactory(
-            RedisStandaloneConfiguration redisStandaloneConfiguration, 
-            JedisPoolConfig jedisPoolConfig) {
-        return new JedisConnectionFactory(redisStandaloneConfiguration);
-    }
-    
-    @Bean
-    public StringRedisSerializer getStringRedisSerializer() {
-        return new StringRedisSerializer();
-    }
-    
-    @Bean
-    public StringRedisTemplate getStringRedisTemplate(
-            JedisConnectionFactory jedisConnectionFactory,
-            StringRedisSerializer stringRedisSerializer) {
-        StringRedisTemplate stringRedisTemplate= new StringRedisTemplate();
-        stringRedisTemplate.setConnectionFactory(jedisConnectionFactory);
-        //redisTemplate.setStringSerializer(stringRedisSerializer);
-        return stringRedisTemplate;
-    }
 
     private final static String EMAIL_SIGN = 
             "<p>------------------</p>"
