90d956f06e6eb407977dccdc3888c1532ed38275
cFrost <cFrost@eulerproject.io>
2019-10-11 23:45:40 +0800

    config

--- euler-boot-demo/src/main/java/org/eulerframework/demo/controller/DemoApi.java
+++ euler-boot-demo/src/main/java/org/eulerframework/demo/controller/DemoApi.java
@@ -3,6 +3,7 @@ package org.eulerframework.demo.controller;
 import org.eulerframework.boot.autoconfigure.web.EulerApplicationProperties;
 import org.eulerframework.web.config.WebConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("demo")
 public class DemoApi {
+    @Autowired
+    private ConfigurableEnvironment configurableEnvironment;
+    @Autowired
+    private EulerApplicationProperties eulerApplicationProperties;
 
     @GetMapping("hello")
     public String hello() {
@@ -21,5 +26,15 @@ public class DemoApi {
     @GetMapping("webConfig")
     public String webConfig() {
         return WebConfig.getRuntimePath();
+    }
+
+    @GetMapping("runtimePath")
+    public String runtimePath() {
+        return eulerApplicationProperties.getRuntimePath();
+    }
+
+    @GetMapping("environment/{key}")
+    public String runtimePath(@PathVariable String key) {
+        return key + ": " + configurableEnvironment.getProperty(key);
     }
 }
