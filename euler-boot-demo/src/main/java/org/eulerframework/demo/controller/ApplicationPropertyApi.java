e131b8f7ef47be5e5dd6c73aed484af96854e0b3
cFrost <cfrost@eulerproject.io>
2019-10-24 20:58:42 +0800

    new controller

--- euler-boot-demo/src/main/java/org/eulerframework/demo/controller/ApplicationPropertyApi.java
+++ euler-boot-demo/src/main/java/org/eulerframework/demo/controller/ApplicationPropertyApi.java
@@ -2,39 +2,24 @@ package org.eulerframework.demo.controller;
 
 import org.eulerframework.boot.autoconfigure.web.EulerApplicationProperties;
 import org.eulerframework.web.config.WebConfig;
+import org.eulerframework.web.core.base.controller.ApiSupportWebController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("demo")
-public class DemoApi {
-    @Autowired
-    private ConfigurableEnvironment configurableEnvironment;
+@RequestMapping("property/application")
+public class ApplicationPropertyApi extends ApiSupportWebController {
     @Autowired
     private EulerApplicationProperties eulerApplicationProperties;
 
-    @GetMapping("hello")
-    public String hello() {
-        return "Euler Boot is running";
-    }
-
-    @GetMapping("webConfig")
-    public String webConfig() {
-        return WebConfig.getRuntimePath();
-    }
-
-    @GetMapping("runtimePath")
-    public String runtimePath() {
-        return eulerApplicationProperties.getRuntimePath();
-    }
-
-    @GetMapping("environment/{key}")
-    public String runtimePath(@PathVariable String key) {
-        return key + ": " + configurableEnvironment.getProperty(key);
+    @GetMapping
+    public EulerApplicationProperties applicationProperties() {
+        return eulerApplicationProperties;
     }
 }
