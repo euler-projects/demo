574a49d368f901eafe30568826dc3cd5a30040c6
cFrost <cFrost@eulerproject.io>
2019-12-01 20:52:19 +0800

    dependence version

--- euler-boot-demo/src/main/java/org/eulerframework/demo/controller/ApplicationPropertyApi.java
+++ euler-boot-demo/src/main/java/org/eulerframework/demo/controller/ApplicationPropertyApi.java
@@ -1,14 +1,9 @@
 package org.eulerframework.demo.controller;
 
-import org.eulerframework.boot.autoconfigure.web.EulerApplicationProperties;
-import org.eulerframework.web.config.WebConfig;
+import org.eulerframework.boot.autoconfigure.property.EulerApplicationProperties;
 import org.eulerframework.web.core.base.controller.ApiSupportWebController;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
