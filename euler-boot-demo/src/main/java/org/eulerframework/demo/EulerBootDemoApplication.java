7ba9bb93e9d2aaa61624f86c9f1c7f02ef2a934f
cFrost <cFrost@eulerproject.io>
2019-12-08 00:18:17 +0800

    instead tabs with spaces

--- euler-boot-demo/src/main/java/org/eulerframework/demo/EulerBootDemoApplication.java
+++ euler-boot-demo/src/main/java/org/eulerframework/demo/EulerBootDemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EulerBootDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EulerBootDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EulerBootDemoApplication.class, args);
+    }
 
 }
